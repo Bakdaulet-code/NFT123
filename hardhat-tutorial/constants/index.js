0f64e909faf057649b77812fee3f3074dfa61a0b
Bakdaulet-Code <zhh_bakdaulet@mail.ru>
2022-10-08 01:32:58 -0700

    FIRST

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x54cD807D7DCaE2ac6c9c8662aBefBC0Bb7c313db";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
